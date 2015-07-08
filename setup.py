5c9df921db5a2a8b57ba32b56343baa486f00527
Elizabeth Myers <elizabeth@interlinked.me>
2015-07-07 21:22:28 -0500

    First beta

--- setup.py
+++ setup.py
@@ -3,20 +3,21 @@
 from setuptools import setup, find_packages
 
 setup(name="taillight",
-      version="0.1a1",
+      version="0.1b1",
       description="An implementation of sigs and slots, with priorities.",
       author="Elizabeth Myers",
       author_email="elizabeth@interlinked.me",
-      url="<none yet>",
+      url="http://github.com/Elizafox/taillight",
       packages=find_packages(exclude=["build", "contrib", "docs", "tests*"]),
       classifiers=[
-          "Development Status :: 3 - Alpha",
-          "Intended Audience :: Developers",
-          "Topic :: Software Development",
-          "Topic :: Software Development :: Libraries :: Python Modules",
-          "Programming Language :: Python :: 3 :: Only",
-          "Programming Language :: Python :: 3.4",
-          "Operating System :: OS Independent",
-          "License :: DFSG approved",
+            "Development Status :: 4 - Beta",
+            "Intended Audience :: Developers",
+            "Topic :: Software Development",
+            "Topic :: Software Development :: Libraries :: Python Modules",
+            "Programming Language :: Python :: 3 :: Only",
+            "Programming Language :: Python :: 3.3",
+            "Programming Language :: Python :: 3.4",
+            "Operating System :: OS Independent",
+            "License :: DFSG approved",
       ]
 )
