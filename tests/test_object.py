73b22a65488f0a55f240c5271944def93a1ecc46
Elizabeth Myers <elizabeth@interlinked.me>
2015-07-11 20:11:17 -0500

    Add tests for UnsharedSignal

--- tests/test_object.py
+++ tests/test_object.py
@@ -11,10 +11,28 @@ class TestSignalObject(unittest.TestCase):
         signal_b2 = signal.Signal("b")
 
         self.assertIs(signal_a, signal_a2)
+        self.assertIs(signal_b, signal_b2)
+        self.assertIsNot(signal_a, signal_b)
 
-    def test_anonymous(self):
+    def test_unshared(self):
+        signal_a = signal.UnsharedSignal("a")
+        signal_b = signal.UnsharedSignal("b")
+        signal_a2 = signal.UnsharedSignal("a")
+        signal_b2 = signal.UnsharedSignal("b")
+
+        self.assertIsNot(signal_a, signal_a2)
+        self.assertIsNot(signal_b, signal_b2)
+        self.assertIsNot(signal_a, signal_b)
+
+    def test_anonymous_signal(self):
         signal_anon1 = signal.Signal()
         signal_anon2 = signal.Signal()
+
+        self.assertIsNot(signal_anon1, signal_anon2)
+
+    def test_anonymous_unshared_signal(self):
+        signal_anon1 = signal.UnsharedSignal()
+        signal_anon2 = signal.UnsharedSignal()
 
         self.assertIsNot(signal_anon1, signal_anon2)
 
