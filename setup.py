3046c4a898819fc310fae1732c1290bc85e480e6
Elizabeth Myers <elizabeth@interlinked.me>
2019-04-27 08:11:07 -0500

    Support Python 3.7

--- setup.py
+++ setup.py
@@ -18,6 +18,7 @@ setup(name="taillight",
           "Programming Language :: Python :: 3.4",
           "Programming Language :: Python :: 3.5",
           "Programming Language :: Python :: 3.6",
+          "Programming Language :: Python :: 3.7",
           "Operating System :: OS Independent",
           "License :: DFSG approved",
       ])
